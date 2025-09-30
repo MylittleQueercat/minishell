dd2a8ed88d184d54d0021649d319ab84a38caa4a
Huilin Guo <hguo@f1br9s4.clusters.42paris.fr>
2025-09-30 15:10:38 +0200

    clean

--- libft/.objs/ft_lstclear_bonus.d
+++
@@ -1,3 +0,0 @@
-.objs/ft_lstclear_bonus.o: srcs/ft_lstclear_bonus.c includes/libft.h
-
-includes/libft.h:
--- libft/.objs/ft_lstmap_bonus.d
+++
@@ -1,3 +0,0 @@
-.objs/ft_lstmap_bonus.o: srcs/ft_lstmap_bonus.c includes/libft.h
-
-includes/libft.h:
